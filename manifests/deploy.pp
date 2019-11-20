c82e26fb58001ce2a322dd96d2c9df532e2a5287
Simon Anderson <simon.anderson111@gmail.com>
2019-11-21 10:24:13 +1100

    added profile class

---
+++ manifests/deploy.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include tomcat_profile::deploy
+class tomcat_profile::deploy {
+}
