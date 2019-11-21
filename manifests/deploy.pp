3d771c320694ec0aae525144187622da9e88451d
root <root@simon-demonix0.classroom.puppet.com>
2019-11-21 02:51:08 +0000

    added java tomcat

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -5,4 +5,15 @@
 # @example
 #   include tomcat_profile::deploy
 class tomcat_profile::deploy {
+
+	include java
+
+	tomcat::install { '/opt/tomcat':
+  		source_url => 'http://apache.mirror.digitalpacific.com.au/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz',
+	}
+
+	tomcat::instance { 'default':
+		catalina_home => '/opt/tomcat',
+	}
+
 }
